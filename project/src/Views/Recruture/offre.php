e34a9a3e8127ed4613d4e2bc7258f3a6a6650376
hamza2024web <hamzaakroubi1528@gmail.com>
2025-01-07 15:26:08 +0100

    finish the part og fetch tags and categories that admin has add in data base

--- project/src/Views/Recruture/offre.php
+++ project/src/Views/Recruture/offre.php
@@ -1,3 +1,18 @@
+<?php
+
+require_once("../../../vendor/autoload.php");
+
+use App\Controllers\tagController;
+use App\Controllers\catcontroller;
+
+$tagControllerFetch = new tagController();
+$resultsTag = $tagControllerFetch->getTag();
+
+$catControllerFetch = new catcontroller();
+$resultsCat = $catControllerFetch->getCat();
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,14 +33,23 @@
         <div class="flex">
             <div class="form-group">
                 <select name="tag" id="tag" required>
-                    <option value="">Tag</option>
-                    <option value="Candidat">Candidat</option>
+                    <option value="">Tags</option>
+                    <?php
+                    foreach ($resultsTag as $resultTag){
+                        ?>
+                        <option value="<?php echo $resultTag['id'] ?>"><?php echo $resultTag["tag_name"]?></option>
+                    <?php } ?>
                 </select>
             </div>
             <div class="form-group">
                 <select name="categorie" id="categorie" required>
-                    <option value="">Categorie</option>
-                    <option value="Candidat">Candidat</option>
+                    <option value="">categories</option>
+                    <?php
+                    foreach ($resultsCat as $resultCat){
+                        ?>
+                        <option value="<?php echo $resultCat['id'] ?>"><?php echo $resultCat["category_name"]?></option>
+                        <?php 
+                    } ?>
                 </select>
             </div>
         </div>
@@ -50,18 +74,18 @@
                 <input name="qualification" type="text" required class="w-full text-sm text-gray-800 border border-gray-300 px-4 py-3 rounded-lg outline-blue-600" placeholder="Enter password" />
             </div>
         </div>
-            <div>
-                <label class="text-gray-800 text-sm mb-2 block">Lieu</label>
-                <div class="relative flex items-center">
-                    <input name="lieu" type="text" class="w-full text-sm text-gray-800 border border-gray-300 px-4 py-3 rounded-lg outline-blue-600" placeholder="Enter skills" />
-                </div>
+        <div>
+            <label class="text-gray-800 text-sm mb-2 block">Lieu</label>
+            <div class="relative flex items-center">
+                <input name="lieu" type="text" class="w-full text-sm text-gray-800 border border-gray-300 px-4 py-3 rounded-lg outline-blue-600" placeholder="Enter skills" />
             </div>
-            <div>
-                <label class="text-gray-800 text-sm mb-2 block">Discription</label>
-                <div class="relative flex items-center">
-                    <input name="discription" type="text" class="w-full text-sm text-gray-800 border border-gray-300 px-4 py-3 rounded-lg outline-blue-600" placeholder="Enter diploma" />
-                </div>
+        </div>
+        <div>
+            <label class="text-gray-800 text-sm mb-2 block">Discription</label>
+            <div class="relative flex items-center">
+                <input name="discription" type="text" class="w-full text-sm text-gray-800 border border-gray-300 px-4 py-3 rounded-lg outline-blue-600" placeholder="Enter diploma" />
             </div>
+        </div>
         <div class="!mt-8">
             <button type="submit" name="submit" class="w-full shadow-xl py-3 px-4 text-sm tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none">
                 Add offre
@@ -71,3 +95,14 @@
 </body>
 
 </html>
+<select name="nationnalite"
+    class="p-2 border border-gray-900 rounded-md bg-gray-800 text-white">
+    <?php
+
+    $nationalities = mysqli_query($con, "select * from nationnality");
+    while ($n = mysqli_fetch_array($nationalities)) {
+    ?>
+        <option value="<?php echo $n['id'] ?>">
+            <?php echo $n['flag'] ?></option>
+    <?php } ?>
+</select>
